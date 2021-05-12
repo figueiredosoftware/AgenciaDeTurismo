fd6792ccf9310483206089ad879bbe7cba9bb098
figueiredosoftware <figueiredosoftware@gmail.com>
2021-05-12 11:12:00 -0300

    commit em 12/05/2021

--- AgenciaDeTurismo/AgenciaDeTurismo/Db/ViagensOnLineDb.cs
+++ AgenciaDeTurismo/AgenciaDeTurismo/Db/ViagensOnLineDb.cs
@@ -1,19 +1,11 @@
-﻿using AgenciaDeTurismo.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
-
 namespace AgenciaDeTurismo.Db
 {
-    public class ViagensOnLineDb:DbContext
+    public class ViagensOnLineDb
     {
-        private const string conexao = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Alessandro\Projetos\Projetos Versionados\AgenciaDeTurismo\AgenciaDeTurismo\AgenciaDeTurismo\App_Data\ViagensOnLineDb.mdf';Integrated Security=True";
-
-        public ViagensOnLineDb() : base(conexao) { }
-
-        public DbSet<Destino> Destinos { get; set; }
     }
 }
