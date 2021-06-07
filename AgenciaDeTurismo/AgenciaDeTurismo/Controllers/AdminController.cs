fff9a1618f60ac188ceb624386d8f59380b26282
figueiredosoftware <figueiredosoftware@gmail.com>
2021-06-07 19:07:31 -0300

    commit 07/06/2021

--- AgenciaDeTurismo/AgenciaDeTurismo/Controllers/AdminController.cs
+++ AgenciaDeTurismo/AgenciaDeTurismo/Controllers/AdminController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.IO;
+using AgenciaDeTurismo.Db;
+using AgenciaDeTurismo.Models;
 
 namespace AgenciaDeTurismo.Controllers
 {
     public class AdminController : Controller
     {
-        private const string ActionDestinoListagem = "DestinoListagem";
+        private const string ActionDestinoListagem = "DestinoListagem"; //name of view DestinoListagem
 
         //Incluir Destino
         [HttpGet]
@@ -29,5 +31,32 @@ namespace AgenciaDeTurismo.Controllers
 
             return nome;
         }
+
+        //Retorna uma instancia de DbContext
+        private ViagensOnLineDb ObterDbContext()
+        {
+            return new ViagensOnLineDb();
+        }
+
+        //Gravar Novo Destino
+        //[HttpPost]
+        //public ActionResult DestinoNovo(Destino destino)
+        //{
+        //    //Se alguma validação falhou
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return View(destino);
+        //    }
+
+        //    //Foto é obrigatória
+        //    if(Request.Files.Count == 0 || Request.Files[0].ContentLength == 0)
+        //    {
+        //        ModelState.AddModelError("", "É necessário enviar uma foto");
+        //        return View(destino);
+        //    }
+
+        //    //Grava
+        //    destino.Foto = GravarFoto(Request);
+        //}
     }
 }
--- AgenciaDeTurismo/AgenciaDeTurismo/Db/ViagensOnLineDb.cs
+++ AgenciaDeTurismo/AgenciaDeTurismo/Db/ViagensOnLineDb.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using AgenciaDeTurismo.Models;
+using System.Data.Entity;
+
 
 namespace AgenciaDeTurismo.Db
 {
-    public class ViagensOnLineDb
+    public class ViagensOnLineDb:DbContext
     {
+        private const string conexao = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename='C:\Alessandro\Projetos\Projetos Versionados\AgenciaDeTurismo\AgenciaDeTurismo\AgenciaDeTurismo\App_Data\ViagensOnLineDb.mdf';Integrated Security = True";
+
+        public ViagensOnLineDb() : base(conexao) 
+        {
+            
+        }
+
+        public DbSet<Destino> Destinos { get; set; }
     }
 }
