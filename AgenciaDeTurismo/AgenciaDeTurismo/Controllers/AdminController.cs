f7f4a2bb7e0db7d3f20fb3dff637e8ec8e476663
figueiredosoftware <figueiredosoftware@gmail.com>
2021-06-14 11:01:40 -0300

    commit 14/06/2021

--- AgenciaDeTurismo/AgenciaDeTurismo/Controllers/AdminController.cs
+++ AgenciaDeTurismo/AgenciaDeTurismo/Controllers/AdminController.cs
@@ -13,11 +13,76 @@ namespace AgenciaDeTurismo.Controllers
     {
         private const string ActionDestinoListagem = "DestinoListagem"; //name of view DestinoListagem
 
-        //Incluir Destino
+        //formulario em branco
         [HttpGet]
         public ActionResult DestinoNovo()
         {
             return View();
+        }
+
+        //Gravar Novo Destino - Incluir Destino
+        [HttpPost]
+        public ActionResult DestinoNovo(Destino destino)
+        {
+            //Se alguma validação falhou
+            if (!ModelState.IsValid)
+            {
+                return View(destino);
+            }
+
+            //Foto é obrigatória
+            if (Request.Files.Count == 0 || Request.Files[0].ContentLength == 0)
+            {
+                ModelState.AddModelError("", "É necessário enviar uma foto");
+                return View(destino);
+            }
+
+            //Grava
+            try
+            {
+                destino.Foto = GravarFoto(Request);
+
+                using (var db = ObterDbContext())
+                {
+                    db.Destinos.Add(destino);
+                    db.SaveChanges();
+                    return RedirectToAction(ActionDestinoListagem);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(destino);
+            }
+        }
+
+        //Listagem
+        [HttpGet]
+        public ActionResult DestinoListagem()
+        {
+            List<Destino> lista = null;
+
+            using (var db = ObterDbContext())
+            {
+                lista = db.Destinos.ToList();
+            }
+
+            return View(lista);
+        }
+
+        //Alterar Destino Get
+        [HttpGet]
+        public ActionResult DestinoAlterar(int id)
+        {
+            using (var db = ObterDbContext())
+            {
+                var destino = db.Destinos.Find(id);
+                if(destino != null)
+                {
+                    return View(destino);
+                }
+            }
+            return RedirectToAction(ActionDestinoListagem);
         }
 
         //Gravar Foto
@@ -26,7 +91,7 @@ namespace AgenciaDeTurismo.Controllers
             string nome = Path.GetFileName(Request.Files[0].FileName);
             string pastaVirtual = "~/Imagens";
             string pathVirtual = pastaVirtual + "/" + nome;
-            string pathFisico = Request.MapPath(pastaVirtual);
+            string pathFisico = Request.MapPath(pathVirtual);
             Request.Files[0].SaveAs(pathFisico);
 
             return nome;
@@ -38,25 +103,5 @@ namespace AgenciaDeTurismo.Controllers
             return new ViagensOnLineDb();
         }
 
-        //Gravar Novo Destino
-        //[HttpPost]
-        //public ActionResult DestinoNovo(Destino destino)
-        //{
-        //    //Se alguma validação falhou
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return View(destino);
-        //    }
-
-        //    //Foto é obrigatória
-        //    if(Request.Files.Count == 0 || Request.Files[0].ContentLength == 0)
-        //    {
-        //        ModelState.AddModelError("", "É necessário enviar uma foto");
-        //        return View(destino);
-        //    }
-
-        //    //Grava
-        //    destino.Foto = GravarFoto(Request);
-        //}
     }
 }
